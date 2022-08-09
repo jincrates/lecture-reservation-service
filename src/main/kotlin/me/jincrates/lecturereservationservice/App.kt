fe6a2999a5b41bfa5db901fd4ab53d00edc08217
jincrates <94jingyu@gmail.com>
2022-08-09 14:38:49 +0900

    공통 엔티티 추가(등록일시, 수정일시)

--- src/main/kotlin/me/jincrates/lecturereservationservice/App.kt
+++ src/main/kotlin/me/jincrates/lecturereservationservice/App.kt
@@ -2,7 +2,9 @@ package me.jincrates.lecturereservationservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableJpaAuditing
 @SpringBootApplication
 class LectureReservationServiceApplication
 
