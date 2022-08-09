99aa7b4971aaff9c42b8dfec89dc8e88500bc57e
jincrates <94jingyu@gmail.com>
2022-08-09 16:18:20 +0900

    임시 사용자 인증여부 추가(개발 진행사항에 따라 사라질 수 있음)

--- src/main/kotlin/me/jincrates/lecturereservationservice/config/WebConfig.kt
+++ src/main/kotlin/me/jincrates/lecturereservationservice/config/WebConfig.kt
@@ -1,7 +1,49 @@
 package me.jincrates.lecturereservationservice.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.MethodParameter
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.support.WebDataBinderFactory
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.method.support.ModelAndViewContainer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
 
 @Configuration
-class WebConfig {
+class WebConfig(
+    private val authUserHandlerArgumentsResolver: AuthUserHandlerArgumentsResolver,
+) : WebMvcConfigurationSupport() {
+
+    override fun addArgumentResolvers(argumentResolvers: MutableList<HandlerMethodArgumentResolver>) {
+        argumentResolvers.apply {
+            add(authUserHandlerArgumentsResolver)
+        }
+    }
 }
+
+@Component
+class AuthUserHandlerArgumentsResolver : HandlerMethodArgumentResolver {
+    override fun supportsParameter(parameter: MethodParameter): Boolean =
+        AuthUser::class.java.isAssignableFrom(parameter.parameterType)
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        mavContainer: ModelAndViewContainer?,
+        webRequest: NativeWebRequest,
+        binderFactory: WebDataBinderFactory?
+    ): Any? {
+
+        return AuthUser(
+            userId = 1,
+            userCode = "94042",
+            username = "사용자"
+        )
+    }
+
+}
+
+data class AuthUser(
+    val userId: Long,
+    val userCode: String,  //사원번호
+    val username: String,  //사원명
+)
